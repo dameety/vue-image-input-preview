958e49ae8401f311314e5288c314becdff37dcd4
Dammy <djdamilola01@gmail.com>
2018-08-14 21:10:52 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="image-box">
+
+      <VueImageInputPreview>
+
+        <!--<div slot="image-preview-template" slot-scope="{chooseImage, image_src, fileName}">-->
+          <!--<p>{{fileName}}</p>-->
+          <!--<img class="image" :src=image_src alt="alt text" style="width: 100px; height: 100px">-->
+
+          <!--<input type="file" class="input" accept="image/*" name="inputName" @change="chooseImage">-->
+        <!--</div>-->
+
+      </VueImageInputPreview>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueImageInputPreview from './components/VueImageInputPreview.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VueImageInputPreview
   }
 }
 </script>
@@ -22,7 +35,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
   margin-top: 60px;
 }
+  .image-box {
+    width: 300px;
+    height: 200px !important;
+  }
 </style>
---
+++ src/components/VueImageInputPreview.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="vue-image-input-preview-container">
+        <slot name="image-preview-template" :chooseImage="chooseImage" :image_src="image_src" :fileName="fileName">
+
+            <div class="body">
+                <img class="image" :src=image_src alt="file" v-if="image_src">
+                <input type="file" accept="image/*" class="input" name="file" @change="chooseImage">
+            </div>
+
+        </slot>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+  export default {
+    name: 'VueImageInputPreview',
+
+      data () {
+          return {
+              image_src: null,
+              fileInput: '',
+              fileName: ''
+          }
+      },
+
+      methods: {
+
+          chooseImage (e) {
+              let files = e.target.files;
+
+              if(files.length === 0) {
+                  this.image_src = null
+                  return
+              }
+
+              this.fileName = e.target.files[0].name
+
+              let reader = new FileReader();
+              reader.onload = (e) => {
+                  this.image_src = e.target.result;
+              }
+
+              reader.readAsDataURL(files[0]);
+          }
+      }
+  }
+</script>
+
+<style scoped>
+    .vue-image-input-preview-container {
+        width: 100%;
+        height: 100%;
+    }
+    .vue-image-input-preview-container .body {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .vue-image-input-preview-container img {
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
